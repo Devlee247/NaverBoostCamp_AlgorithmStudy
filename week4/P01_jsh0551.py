29b9dfdf1176af6b1f755fe1c2304408992713ac
jsh0551 <jsh941108@gmail.com>
2022-02-12 20:32:26 +0900

    Update P01_jsh0551.py

--- week4/P01_jsh0551.py
+++ week4/P01_jsh0551.py
@@ -1,33 +1,33 @@
 def transform_count(count,len_word):
-    if count==1:
+    if count == 1:
         return len_word
     else:
-        return len_word+len(str(count))
+        return len_word + len(str(count))
 
 def compress_word(string,len_word):
-    s=string
-    std_word=s[:len_word]
-    count=0
-    result=0
+    s = string
+    std_word = s[:len_word]
+    count = 0
+    result = 0
     while True:
-        if len_word>len(s):
-            result+=len(s)+transform_count(count,len_word)
+        if len_word > len(s):
+            result += len(s) + transform_count(count,len_word)
             break
-        if std_word==s[:len_word]:
-            count+=1
-            s=s[len_word:]
+        if std_word == s[:len_word]:
+            count += 1
+            s = s[len_word:]
         else:
-            std_word=s[:len_word]
-            result+=transform_count(count,len_word)
-            count=0
+            std_word = s[:len_word]
+            result += transform_count(count,len_word)
+            count = 0
     return result
 
 
 def solution(s):
-    answer=1001
-    result=0
-    len_string=(len(s)+1)//2
+    answer = 1001
+    result = 0
+    len_string = (len(s) + 1) // 2
     for n in range(len_string):
-        result=compress_word(s,n+1)
-        answer=min(answer,result)
+        result = compress_word(s,n + 1)
+        answer = min(answer,result)
     return answer
